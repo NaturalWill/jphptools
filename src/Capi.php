f3cd7c4e24818051ebdbebec4947872f3c201d26
Jier Zhang <naturalwill999@gmail.com>
2016-05-28 16:38:02 +0800

    use composer

---
+++ src/Capi.php
@@ -0,0 +1,49 @@
+<?php
+class Capi {
+	static function mkjson($response = '', $callback = '') {
+		global $_SGLOBAL;
+		$response = empty ( $response ) ? $_SGLOBAL ['mresponse'] : $response;
+		if ($callback) {
+			header ( 'Cache-Control: no-cache, must-revalidate' );
+			header ( 'Content-Type: text/javascript;charset=utf-8' );
+			echo $callback . '(' . json_encode ( $response ) . ');';
+		} else {
+			// application/x-json will make error in iphone, so I use the text/json
+			// instead of the orign mine type
+			header ( 'Cache-Control: no-cache, must-revalidate' );
+			header ( 'Content-Type: text/json;' );
+			
+			echo json_encode ( $response );
+		}
+		exit ();
+	}
+	public static function showmessage_by_data($msgkey, $code = 1, $data = array()) {
+		obclean ();
+		
+		// 语言
+		$msglang = include __DIR__ . DIRECTORY_SEPARATOR . 'lang_showmessage.php';
+		if (isset ( $msglang [$msgkey] )) {
+			$message = self::lang_replace ( $msglang [$msgkey], $values );
+		} else {
+			$message = $msgkey;
+		}
+		$r = array ();
+		$r ['code'] = $code;
+		$r ['data'] = $data;
+		$r ['msg'] = $message;
+		$r ['action'] = $msgkey;
+		self::mkjson ( $r, $_REQUEST ['callback'] );
+	}
+	
+	// 语言替换
+	static function lang_replace($text, $vars) {
+		if ($vars) {
+			foreach ( $vars as $k => $v ) {
+				$rk = $k + 1;
+				$text = str_replace ( '\\' . $rk, $v, $text );
+			}
+		}
+		return $text;
+	}
+}
+?>
